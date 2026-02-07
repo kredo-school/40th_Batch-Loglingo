d5dabbab8c103a221801105a76d61787feb7b9af
Haruka-723 <upperspacchi@yahoo.co.jp>
2026-02-07 14:32:51 +0900

    UI / *(file name) [Changes] - Created compinents ;profile-show-questions / profile-show-following - Profile : added components

---
+++ resources/views/components/profile-show-following.blade.php
@@ -0,0 +1,49 @@
+<!-- show questions  -->
+
+ <div class="space-y-2">
+
+    <!-- user profile example1 -->
+    <div class="bg-white rounded-[1rem] shadow-sm border border-gray-100 p-3">
+      <div class="flex items-center justify-between">
+        <div class="flex items-center space-x-3">
+          <div class="w-14 h-14 rounded-full bg-pink-400 border-2 border-white shadow-sm overflow-hidden">
+            <img src="#" alt="user" class="w-full h-full object-cover">
+          </div>
+          <div>
+            <p class="text-[16px] font-bold">User name A</p>
+            <p class="text-[15px]"><i class="fa-solid fa-message text-gray-600"></i> <span>Japanese</span></p>
+            <p class="text-[15px]"><i class="fa-solid fa-pen text-gray-600"></i> <span>English</span></p>
+          </div>
+        </div>
+        <button class="bg-[#B178CC] text-white text-[13px] font-bold px-3 py-1 rounded-full shadow-md hover:bg-[#a068ba] transition-all hover:scale-105 active:scale-95">
+          Follow
+        </button>
+      </div>
+    </div>
+
+</div>
+
+
+
+<div class="space-y-2">
+
+    <!-- user profile example1 -->
+    <div class="bg-white rounded-[1rem] shadow-sm border border-gray-100 p-3">
+      <div class="flex items-center justify-between">
+        <div class="flex items-center space-x-3">
+          <div class="w-14 h-14 rounded-full bg-pink-400 border-2 border-white shadow-sm overflow-hidden">
+            <img src="#" alt="user" class="w-full h-full object-cover">
+          </div>
+          <div>
+            <p class="text-[16px] font-bold">User name A</p>
+            <p class="text-[15px]"><i class="fa-solid fa-message text-gray-600"></i> <span>Japanese</span></p>
+            <p class="text-[15px]"><i class="fa-solid fa-pen text-gray-600"></i> <span>English</span></p>
+          </div>
+        </div>
+        <button class="bg-[#B178CC] text-white text-[13px] font-bold px-3 py-1 rounded-full shadow-md hover:bg-[#a068ba] transition-all hover:scale-105 active:scale-95">
+          Follow
+        </button>
+      </div>
+    </div>
+
+</div>
